50d7ccb9ef58298fa0656325a3fe300afbee0660
Bradley Jensen <bradley13@gmail.com>
2019-03-06 21:30:14 -0500

    initial functionality complete

--- word-guess.js
+++ word-guess.js
@@ -1,26 +1,121 @@
 var Word = require(`./Word`);
 var inquirer = require(`inquirer`);
 
-// generate a random word
-var randomWord = new Word("hurrdurr");
+var alphabet = [
+  `a`,
+  `b`,
+  `c`,
+  `d`,
+  `e`,
+  `f`,
+  `g`,
+  `h`,
+  `i`,
+  `j`,
+  `k`,
+  `l`,
+  `m`,
+  `n`,
+  `o`,
+  `p`,
+  `q`,
+  `r`,
+  `s`,
+  `t`,
+  `u`,
+  `v`,
+  `w`,
+  `x`,
+  `y`,
+  `z`
+];
+var randomWord = {};
+var done = false;
+var lives = 10;
+var totalScore = 0;
 
-// prompt the user to guess a letter
+initializeGame = function() {
+  // generate a random word
+  randomWord = new Word("hurrdurr");
+  done = false;
+  lives = 10;
+};
 
-inquirer
-  .prompt({
-    name: "letter",
-    type: "input",
-    message: "Type a letter to guess..."
-  })
-  .then(function(answer) {
-    console.log(`guess: ${answer.letter}`);
-    randomWord.guess(answer.letter);
-    refresh();
-  });
+let playRound = function() {
+  printWordStatus();
+  if (!done) {
+    inquirer
+      .prompt({
+        name: "letter",
+        type: "input",
+        message: "Type a letter to guess...",
+        validate: function(value) {
+          if (alphabet.indexOf(value.toLowerCase()) >= 0) {
+            return true;
+          } else {
+            return "Please enter a single letter...";
+          }
+        }
+      })
+      .then(function(answer) {
+        console.log(`---------------------------------------`);
+        let goodGuess = randomWord.guess(answer.letter.toLowerCase());
+        if (goodGuess) {
+          console.log(`Good Guess!!!`);
+        } else {
+          lives--;
+          console.log(`Wrong!!! ${lives} remaining...`);
+        }
+        if (lives <= 0 || randomWord.isComplete()) {
+          done = true;
+        }
+        playRound();
+      });
+  } else {
+    roundComplete();
+  }
+};
 
-let refresh = function() {
+let roundComplete = function() {
+  // tell the user they won/lost
+  if (lives > 0 && randomWord.isComplete()) {
+    // winner
+    totalScore++;
+    console.log(`You Won!!! Total Score: ${totalScore}`);
+  } else {
+    // loser
+    totalScore--;
+    console.log(`You Lost!!! Total Score: ${totalScore}`);
+  }
+
+  // prompt if they want to play again or quit
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: ["Start Another Round", "Quit"]
+    })
+    .then(function(answer) {
+      switch (answer.action) {
+        case "Start Another Round":
+          initializeGame();
+          playRound();
+          break;
+
+        case "Quit":
+        console.log(`Thank you for playing! Good Bye :)`)
+        break;
+      }
+    });
+};
+
+let printWordStatus = function() {
   // show the user the blanks
   console.log(`---------------------------------------`);
   console.log(`Word Status: "${randomWord.toString()}"`);
   console.log(`---------------------------------------`);
 };
+
+initializeGame();
+playRound();
--- word.js
+++ word.js
@@ -19,10 +19,25 @@ function Word(word) {
 
   this.guess = function(letter) {
     // iterate through letters and guess everything
+    let guess = false; 
     for(let i=0; i<this.letters.length; i++) {
-      this.letters[i].guessValue(letter);
+      if( this.letters[i].guessValue(letter) ) {
+        guess = true;
+      }
     }
+    return guess;
   };
+
+  this.isComplete = function() {
+    // return true if all letters are guessed
+    let complete = true;
+    for(let i=0; i<this.letters.length; i++) {
+      if(!this.letters[i].guessed) {
+        complete = false;
+      }
+    }
+    return complete;
+  }
 }
 
 module.exports = Word;
